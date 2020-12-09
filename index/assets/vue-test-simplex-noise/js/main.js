f30b8ad5da9233255b71ce4801811933f8a69f19
bemuse01 <31756306+bemuse01@users.noreply.github.com>
2020-12-10 03:02:31 +0900

    Add files via upload

--- index/assets/vue-test-simplex-noise/js/main.js
+++ index/assets/vue-test-simplex-noise/js/main.js
@@ -3,20 +3,22 @@ new Vue({
     data(){
         return{
             util: {
-                width: window.innerWidth,
-                height: window.innerHeight,
+                width: window.innerWidth / 2,
+                height: window.innerHeight / 2,
                 simplex: new SimplexNoise()
             },
             style: {
                 wrap: {width: '100vw', height: '100vh', left: '0', top: '0'},
-                back: {width: '100vw', height: '100vh'}
+                back: {width: '100vw', height: '100vh'},
+                frame: {width: '100vw', height: '100vh'}
             },
             param: {
-                w: 0.05,
-                h: 0.05,
+                w: 0.01,
+                h: 0.01,
                 smooth: 200,
-                rd: 0.00008,
-                ratio: 2188 / 3500
+                velocity: 0.00075,
+                ratio: 2188 / 3500,
+                range: 0.5,
             },
             back: {
                 x: 0,
@@ -37,6 +39,7 @@ new Vue({
         // init
         init(){
             this.setBackgroundSize()
+            this.setFrameSize()
             this.animate()
 
             window.addEventListener('resize', this.onWindowResize, false)
@@ -45,43 +48,44 @@ new Vue({
 
         // background
         setBackgroundSize(){
-            const width = this.util.width * this.param.w
+            const width = this.util.width * this.param.w, oWidth = this.util.width + (width * 2)
+            const oHeight = oWidth * this.param.ratio, height = oHeight * this.param.h
 
-            const oWidth = this.util.width + (width * 2), oHeight = oWidth * this.param.ratio, height = oHeight * this.param.h
-
-            // this.style.wrap.width = this.util.width + (width * 2) + 'px'
-            // this.style.wrap.height = this.util.height + (height * 2) + 'px'
-            // this.style.wrap.left = (this.util.width - (this.util.width + (width * 2))) / 2 + 'px'
-            // this.style.wrap.top = (this.util.height - (this.util.height + (height * 2))) / 2 + 'px'
             this.style.wrap.width = oWidth + 'px'
             this.style.wrap.height = oHeight + 'px'
-            this.style.wrap.left = (this.util.width - oWidth) / 2 + 'px'
-            this.style.wrap.top = (this.util.height - oHeight) / 2 + 'px'
+            this.style.wrap.left = (this.util.width * 2 - oWidth) / 2 + 'px'
+            this.style.wrap.top = (this.util.height * 2 - oHeight) / 2 + 'px'
+            // this.style.wrap.left = (this.util.width - oWidth) / 2 + 'px'
+            // this.style.wrap.top = (this.util.height - oHeight) / 2 + 'px'
 
             this.style.back.width = oWidth + 'px'
             this.style.back.height = oHeight + 'px'
 
-            // console.log(this.util.width * this.param.w * 2)
-            // console.log(this.util.width * this.param.w)
-            // console.log(this.util.width)
             console.log(width, oWidth)
         },
         moveBackground(time){
-            const width = this.util.width * this.param.w
-
-            const oWidth = this.util.width + (width * 2), oHeight = oWidth * this.param.ratio, height = oHeight * this.param.h
+            const width = this.util.width * this.param.w, oWidth = this.util.width + (width * 2)
+            const oHeight = oWidth * this.param.ratio, height = oHeight * this.param.h
             
-            this.back.x = this.util.simplex.noise2D(time * 0.02 / this.param.smooth, time * this.param.rd) * width * 0.5
-            this.back.y = this.util.simplex.noise2D(time * 0.01 / this.param.smooth, time * this.param.rd) * height * 0.5
+            this.back.x = this.util.simplex.noise2D(time * 0.02 / this.param.smooth, time * this.param.velocity) * width * this.param.range
+            this.back.y = this.util.simplex.noise2D(time * 0.01 / this.param.smooth, time * this.param.velocity) * height * this.param.range
+        },
+
+
+        // frame
+        setFrameSize(){
+            this.style.frame.width = this.util.width + 'px'
+            this.style.frame.height = this.util.height + 'px'
         },
 
 
         // window event
         onWindowResize(){
-            this.util.width = window.innerWidth
-            this.util.height = window.innerHeight
+            this.util.width = window.innerWidth / 2
+            this.util.height = window.innerHeight / 2
 
             this.setBackgroundSize()
+            this.setFrameSize()
         },
 
 
